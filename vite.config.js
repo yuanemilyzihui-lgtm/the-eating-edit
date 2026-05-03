c06013926d7b28298a766b3cf2efa6d62ea477ed
rachelyuan <zihui_yuan@brown.edu>
2026-05-02 23:04:17 -0400

    include admin and portal pages in vite build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        admin: 'admin.html',
+        portal: 'dietician-portal.html',
+      }
+    }
+  }
 })
